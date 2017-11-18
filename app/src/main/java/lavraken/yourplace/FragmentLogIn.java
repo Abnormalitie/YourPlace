d64da812c9f4ad3c863181c748c9d0b8a990527e
Abnormalitie <Psohnchristensen@gmail.com>
2017-11-18 21:01:11 +0100

    Made fragments work

---
+++ app/src/main/java/lavraken/yourplace/FragmentLogIn.java
@@ -0,0 +1,21 @@
+package lavraken.yourplace;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Palle on 18/11/2017.
+ */
+
+public class FragmentLogIn extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+}
--- app/src/main/java/lavraken/yourplace/MainPage.java
+++ app/src/main/java/lavraken/yourplace/MainPage.java
@@ -1,21 +1,153 @@
 package lavraken.yourplace;
 
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainPage extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainPage extends AppCompatActivity {
+
+        private DrawerLayout mDrawer;
+        private Toolbar toolbar;
+        private NavigationView nvDrawer;
+        private Class fragmentC;
+        private Fragment fragment1;
+
+        private ActionBarDrawerToggle drawerToggle;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            try {
+                fragmentC = FragmentMain.class;
+                fragment1 = (Fragment) fragmentC.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            if(savedInstanceState == null) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.flContent, fragment1);
+                transaction.commit();
+
+            }
+
+            // Set a Toolbar to replace the ActionBar.
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            // Find our drawer view
+            nvDrawer = (NavigationView) findViewById(R.id.nvView);
+
+            // Setup drawer view
+            setupDrawerContent(nvDrawer);
+
+            mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawerToggle = setupDrawerToggle();
+
+            // Tie DrawerLayout events to the ActionBarToggle
+            mDrawer.addDrawerListener(drawerToggle);
+
+            // Setup drawer view
+            setupDrawerContent(nvDrawer);
+
+            View headerLayout = nvDrawer.getHeaderView(0);
+        }
+
+        @Override
+        protected void onPostCreate(Bundle savedInstanceState) {
+            super.onPostCreate(savedInstanceState);
+            // Sync the toggle state after onRestoreInstanceState has occurred.
+            drawerToggle.syncState();
+        }
+
+        @Override
+        public void onConfigurationChanged(Configuration newConfig) {
+            super.onConfigurationChanged(newConfig);
+            // Pass any configuration change to the drawer toggles
+            drawerToggle.onConfigurationChanged(newConfig);
+        }
+
+        private ActionBarDrawerToggle setupDrawerToggle() {
+            // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+            // and will not render the hamburger icon without it.
+            return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+        }
+
+        private void setupDrawerContent(NavigationView navigationView) {
+            navigationView.setNavigationItemSelectedListener(
+                    new NavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(MenuItem menuItem) {
+                            selectDrawerItem(menuItem);
+                            return true;
+                        }
+                    });
+        }
+
+        public void selectDrawerItem(MenuItem menuItem) {
+            // Create a new fragment and specify the fragment to show based on nav item clicked
+            Fragment fragment = null;
+            Class fragmentClass;
+            switch(menuItem.getItemId()) {
+                case R.id.nav_book:
+                    fragmentClass = FragmentBook.class;
+                    break;
+                case R.id.nav_contact:
+                    fragmentClass = FragmentContact.class;
+                    break;
+                case R.id.nav_about:
+                    fragmentClass = FragmentAbout.class;
+                    break;
+                case R.id.nav_staff:
+                    fragmentClass = FragmentLogIn.class;
+                    break;
+                default:
+                    fragmentClass = FragmentBook.class;
+            }
+
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+
+            transaction.replace(R.id.flContent, fragment).commit();
+            transaction.addToBackStack(null);
+
+            // Highlight the selected item has been done by NavigationView
+            menuItem.setChecked(true);
+            // Set action bar title
+            setTitle(menuItem.getTitle());
+            // Close the navigation drawer
+            mDrawer.closeDrawers();
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            if (drawerToggle.onOptionsItemSelected(item)) {
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+}
+        /*implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,4 +233,4 @@ public class MainPage extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-}
+}*/
