cab64f9b3e4b5247a1dd76c6a69925cf284d75cc
Legio17 <rokuro07@gmail.com>
2017-11-19 17:48:42 +0100

    LogIn implementation (can't find correct layout error)

--- app/src/main/java/lavraken/yourplace/FragmentLogIn.java
+++ app/src/main/java/lavraken/yourplace/FragmentLogIn.java
@@ -3,12 +3,19 @@ package lavraken.yourplace;
 import android.app.Application;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Palle on 18/11/2017.
@@ -17,9 +24,12 @@ import android.widget.EditText;
 public class FragmentLogIn extends Fragment {
 
     //For LogIn
-    public EditText logInEmailField;
-    public EditText logInPasswordField;
-    public Button logInButton;
+    private EditText logInEmailField;
+    private EditText logInPasswordField;
+
+    private Button logInButton;
+
+    private FirebaseAuth logInAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,11 +37,36 @@ public class FragmentLogIn extends Fragment {
 
         setContentView(R.layout.fragment_log_in);
 
+        logInAuth = FirebaseAuth.getInstance();
+
         logInEmailField = (EditText) findViewById(R.id.logInEmail);
         logInPasswordField = (EditText) findViewById(R.id.logInPassword);
         logInButton = (Button) findViewById(R.id.LogInButton);
 
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startSignIn();
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+
+    private void startSignIn() {
+        String email = logInEmailField.getText().toString();
+        String password = logInPasswordField.getText().toString();
+
+        logInAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(!task.isSuccessful()){
+                    Toast.makeText(MainPage.this, "SignInProblem", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
