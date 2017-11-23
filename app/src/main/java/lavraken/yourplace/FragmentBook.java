1f93f35a9cd7f3617d48162cb35135b878212395
Legio17 <rokuro07@gmail.com>
2017-11-23 21:22:11 +0100

    Booking system

--- app/src/main/java/lavraken/yourplace/FragmentBook.java
+++ app/src/main/java/lavraken/yourplace/FragmentBook.java
@@ -1,7 +1,6 @@
 package lavraken.yourplace;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,12 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.jar.Attributes;
 
 /**
  * Created by Palle on 18/11/2017.
@@ -24,11 +24,16 @@ import com.google.firebase.database.ValueEventListener;
 
 public class FragmentBook extends Fragment {
 
-    private EditText bookDateField;
+    private EditText bookDayField;
+    private EditText bookMonthField;
     private EditText bookTimeField;
-    private TextView bookDataView;
+    private EditText bookNameField;
+    private EditText bookProcedureField;
+
+    private TextView bookPriceView;
 
     private Button bookSubmitButton;
+
 
     DatabaseReference mRootReference = FirebaseDatabase.getInstance().getReference();
     DatabaseReference mConditionRef = mRootReference.child("condition");
@@ -37,9 +42,12 @@ public class FragmentBook extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        bookDateField = (EditText) view.findViewById(R.id.bookDateField);
+        bookDayField = (EditText) view.findViewById(R.id.bookDayField);
+        bookMonthField = (EditText) view.findViewById(R.id.bookMonthField);
         bookTimeField = (EditText) view.findViewById(R.id.bookTimeField);
-        bookDataView = (TextView) view.findViewById(R.id.bookDataView);
+        bookNameField = (EditText) view.findViewById(R.id.bookNameField);
+        bookProcedureField = (EditText) view.findViewById(R.id.bookProcedureField);
+        bookPriceView = (TextView) view.findViewById(R.id.bookPriceView);
 
         bookSubmitButton = (Button) view.findViewById(R.id.bookSubmitButton);
 
@@ -59,8 +67,8 @@ public class FragmentBook extends Fragment {
         mConditionRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String text = dataSnapshot.getValue(String.class);
-                bookDataView.setText(text);
+                //String text = dataSnapshot.getValue(String.class);
+                //bookDataView.setText(text);
             }
 
             @Override
@@ -72,7 +80,14 @@ public class FragmentBook extends Fragment {
         bookSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mConditionRef.setValue("Foggy");
+                //mConditionRef.setValue("Foggy");
+                //mConditionRef.push().setValue("pushtest");
+                String date = bookDayField.getText()+"-"+bookMonthField.getText();
+                String time = bookTimeField.getText().toString();
+                String name = bookNameField.getText().toString();
+                String procedure = bookProcedureField.getText().toString();
+
+                mConditionRef.child(date).child(time).setValue(procedure,name);
             }
         });
     }
